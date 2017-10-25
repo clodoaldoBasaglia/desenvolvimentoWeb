a0846432e5afcd609d9b0c430e98f2fde0d62a99
clodoaldoBasaglia <clodoaldofonseca92@gmail.com>
2017-10-25 14:52:24 -0200

    Login caminhando

--- WebService/src/webservice/OrdoProcessios.java
+++ WebService/src/webservice/OrdoProcessios.java
@@ -30,8 +30,10 @@ public class OrdoProcessios implements Runnable {
     private final Socket sok;
     InputStream input;
     OutputStream output;
-    private boolean isLogado = true;
+    private boolean isLogado = false;
     String pathToHtml = new File("").getAbsolutePath();
+    Arquivo arq = null;
+    Headers headers = new Headers();
 
     OrdoProcessios(Socket sok) {
         this.sok = sok;
@@ -40,117 +42,26 @@ public class OrdoProcessios implements Runnable {
     private void codex(InputStream input) {
 
         Headers headers = new Headers();
-
+        System.out.println(this.isLogado);
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(input));
         try {
-            Arquivo arq = null;
+
             String request = bufferedReader.readLine();
             String texto = "";
             Request pedido = new Request();
             System.out.println("primeira linha: " + request);
             ArrayList<String> aux = new ArrayList<String>();
-            if (request.split(" ")[1].equalsIgnoreCase("/") || request.split(" ")[1].equalsIgnoreCase("/bemvindo.html")
-                    || request.split(" ")[1].equalsIgnoreCase("/bemvindo")) {
-                salvarRequisicoes("/bemvindo");
-                arq = new Arquivo(this.pathToHtml + "/src/html/bemvindo.html");
-                this.output.write(headers.BasicHeader().getBytes());
-                this.output.write(arq.openFile().getBytes());
-                // || request.split(" ")[1].equalsIgnoreCase("/bemvindo.html")
-            } else if (request.split(" ")[1].equalsIgnoreCase("/login")) {
-                salvarRequisicoes("/login");
-                arq = new Arquivo(this.pathToHtml + "/src/html/login.html");
-                this.output.write(headers.BasicHeader().getBytes());
-                this.output.write(arq.openFile().getBytes());
-            } else if (request.split(" ")[1].equalsIgnoreCase("/login2")) {
-                salvarRequisicoes("/login2");
-                arq = new Arquivo(this.pathToHtml + "/src/html/login2.html");
-                this.output.write(headers.BasicHeader().getBytes());
-                this.output.write(arq.openFile().getBytes());
-            } else if (request.split(" ")[1].equalsIgnoreCase("/login2?")) {
-                salvarRequisicoes("/login2");
-                if (this.isLogado) {
-                    this.output.write(headers.BasicHeader().getBytes());
-                    arq = new Arquivo(this.pathToHtml + "/src/html/diretorios.html");
-                    String lerDiretorio = arq.lerDiretorio(this.pathToHtml);
-                    String diretorio = arq.openFile();
-                    diretorio = diretorio.replaceAll("panzerkampfwagen", lerDiretorio);
-                    this.output.write(diretorio.getBytes());
-                } else {
-                    acessoNegado();
-                }
-            } else if (request.contains("/diretorios.html") || request.contains("/diretorios")) {
-                salvarRequisicoes("/diretorios");
-                if (this.isLogado) {
-                    this.output.write(headers.BasicHeader().getBytes());
-                    arq = new Arquivo(this.pathToHtml + "/src/html/diretorios.html");
-                    String lerDiretorio = arq.lerDiretorio(this.pathToHtml);
-                    String diretorio = arq.openFile();
-                    diretorio = diretorio.replaceAll("panzerkampfwagen", lerDiretorio);
-                    this.output.write(diretorio.getBytes());
-                } else {
-                    acessoNegado();
-                }
-            } else if (request.contains("/leArquivo/") || request.contains("/leArquivo")) {
-                salvarRequisicoes("/leArquivo");
-                if (this.isLogado) {
-                    this.output.write(headers.BasicHeader().getBytes());
-                    arq = new Arquivo(this.pathToHtml + "/src/html/diretorios.html");
-                    String caminho = request.split(" ")[1];//.substring(request.indexOf("/"), request.split(" ")[1].length());
-                    caminho = caminho.substring(1, request.split(" ")[1].length());
-                    caminho = caminho.substring(caminho.indexOf("/"), caminho.length());
-                    String lerDiretorio = arq.lerDiretorio(caminho + "/");
-                    String diretorio = arq.openFile();
-                    diretorio = diretorio.replaceAll("panzerkampfwagen", lerDiretorio);
-                    this.output.write(diretorio.getBytes());
-                } else {
-                    acessoNegado();
-                }
-            } else if (request.contains("/telemetria.html") || request.contains("/telemetria")) {
-                salvarRequisicoes("/telemetria");
-                if (this.isLogado) {
-                    this.output.write(headers.BasicHeader().getBytes());
-                    arq = new Arquivo(this.pathToHtml + "/src/html/telemetria.html");
-                    String openFile = arq.openFile();
-                    String replace = openFile.replace("panzerkampfwagen", arq.aboutServer());
-                    this.output.write(replace.getBytes());
-                } else {
-                    acessoNegado();
-                }
-            } else if (request.contains("/historico.html") || request.contains("/historico")) {
-                salvarRequisicoes("/historico");
-                if (this.isLogado) {
-                    this.output.write(headers.BasicHeader().getBytes());
-                    arq = new Arquivo(this.pathToHtml + "/src/html/historico.html");
-                    String openFile = arq.openFile();
-                    String info = getInfoRequisicoes();
-                    String replace = openFile.replace("panzerkampfwagen", info);
-                    this.output.write(replace.getBytes());
-                } else {
-                    acessoNegado();
-                }
-            } else if (request.contains("POST") || request.contains("/loginFunction")) {
-                this.output.write(headers.BasicHeader().getBytes());
-                arq = new Arquivo(this.pathToHtml + "/src/html/blank.html");
-                this.output.write(arq.openFile().getBytes());
-            } else {
-                arq = new Arquivo(this.pathToHtml + "/src/html/erro404.html");
-                this.output.write(headers.BasicHeader().getBytes());
-                this.output.write(arq.openFile().getBytes());
-
+            if (request.contains("POST")) {
+                metodoPOST(request,bufferedReader);
+            } else if (request.contains("GET")) {
+                System.out.println(this.isLogado);
+                metodoGET(request);
             }
-            if (request.contains("GET")) {
-                String helper = "";
-                while ((helper = bufferedReader.readLine()) != null) {
-                    if (helper.contains("login=")) {
-                        System.out.println("LOGIN get");
-                    }
-                }
 
-            }
         } catch (IOException ex) {
             Logger.getLogger(OrdoProcessios.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println("aqu");
-            fazLogin(bufferedReader);
+            
         }
     }
 
@@ -169,6 +80,7 @@ public class OrdoProcessios implements Runnable {
     private void acessoNegado() {
         Arquivo arq;
         Headers headers = new Headers();
+        System.out.println(this.isLogado);
         try {
             this.output.write(headers.BasicHeader().getBytes());
             arq = new Arquivo(this.pathToHtml + "/src/html/acessoNegado.html");
@@ -191,6 +103,7 @@ public class OrdoProcessios implements Runnable {
                         if (login.equalsIgnoreCase("root") && senha.equalsIgnoreCase("root")) {
                             this.isLogado = true;
                             System.out.println("logou");
+                            System.out.println(this.isLogado);
                             return true;
                         } else {
                             return false;
@@ -223,7 +136,7 @@ public class OrdoProcessios implements Runnable {
                         cont++;
                     }
                 }
-                linhas = "<tr><td>" + num + "</td><td>"+ar.get(i)+"</td><td>"+cont+"</td></tr>";
+                linhas = "<tr><td>" + num + "</td><td>" + ar.get(i) + "</td><td>" + cont + "</td></tr>";
                 num++;
             }
         } catch (FileNotFoundException ex) {
@@ -255,6 +168,108 @@ public class OrdoProcessios implements Runnable {
             } catch (IOException ex) {
                 Logger.getLogger(OrdoProcessios.class.getName()).log(Level.SEVERE, null, ex);
             }
+        }
+    }
+
+    private void metodoGET(String request) throws IOException {
+        System.out.println("Metodo get: "+this.isLogado);
+        if (request.split(" ")[1].equalsIgnoreCase("/") || request.split(" ")[1].equalsIgnoreCase("/bemvindo.html")
+                || request.split(" ")[1].equalsIgnoreCase("/bemvindo")) {
+            salvarRequisicoes("/bemvindo");
+            arq = new Arquivo(this.pathToHtml + "/src/html/bemvindo.html");
+            this.output.write(headers.BasicHeader().getBytes());
+            this.output.write(arq.openFile().getBytes());
+            // || request.split(" ")[1].equalsIgnoreCase("/bemvindo.html")
+        } else if (request.split(" ")[1].equalsIgnoreCase("/login")) {
+            salvarRequisicoes("/login");
+            arq = new Arquivo(this.pathToHtml + "/src/html/login.html");
+            this.output.write(headers.BasicHeader().getBytes());
+            this.output.write(arq.openFile().getBytes());
+        } else if (request.split(" ")[1].equalsIgnoreCase("/login2")) {
+            salvarRequisicoes("/login2");
+            arq = new Arquivo(this.pathToHtml + "/src/html/login2.html");
+            this.output.write(headers.BasicHeader().getBytes());
+            this.output.write(arq.openFile().getBytes());
+        } else if (request.split(" ")[1].equalsIgnoreCase("/login2?")) {
+            salvarRequisicoes("/login2");
+            if (this.isLogado) {
+                this.output.write(headers.BasicHeader().getBytes());
+                arq = new Arquivo(this.pathToHtml + "/src/html/diretorios.html");
+                String lerDiretorio = arq.lerDiretorio(this.pathToHtml);
+                String diretorio = arq.openFile();
+                diretorio = diretorio.replaceAll("panzerkampfwagen", lerDiretorio);
+                this.output.write(diretorio.getBytes());
+            } else {
+                acessoNegado();
+            }
+        } else if (request.contains("/diretorios.html") || request.contains("/diretorios")) {
+            salvarRequisicoes("/diretorios");
+            if (this.isLogado) {
+                this.output.write(headers.BasicHeader().getBytes());
+                arq = new Arquivo(this.pathToHtml + "/src/html/diretorios.html");
+                String lerDiretorio = arq.lerDiretorio(this.pathToHtml);
+                String diretorio = arq.openFile();
+                diretorio = diretorio.replaceAll("panzerkampfwagen", lerDiretorio);
+                this.output.write(diretorio.getBytes());
+            } else {
+                acessoNegado();
+            }
+        } else if (request.contains("/leArquivo/") || request.contains("/leArquivo")) {
+            salvarRequisicoes("/leArquivo");
+            if (this.isLogado) {
+                this.output.write(headers.BasicHeader().getBytes());
+                arq = new Arquivo(this.pathToHtml + "/src/html/diretorios.html");
+                String caminho = request.split(" ")[1];//.substring(request.indexOf("/"), request.split(" ")[1].length());
+                caminho = caminho.substring(1, request.split(" ")[1].length());
+                caminho = caminho.substring(caminho.indexOf("/"), caminho.length());
+                String lerDiretorio = arq.lerDiretorio(caminho + "/");
+                String diretorio = arq.openFile();
+                diretorio = diretorio.replaceAll("panzerkampfwagen", lerDiretorio);
+                this.output.write(diretorio.getBytes());
+            } else {
+                acessoNegado();
+            }
+        } else if (request.contains("/telemetria.html") || request.contains("/telemetria")) {
+            salvarRequisicoes("/telemetria");
+            if (this.isLogado) {
+                this.output.write(headers.BasicHeader().getBytes());
+                arq = new Arquivo(this.pathToHtml + "/src/html/telemetria.html");
+                String openFile = arq.openFile();
+                String replace = openFile.replace("panzerkampfwagen", arq.aboutServer());
+                this.output.write(replace.getBytes());
+            } else {
+                acessoNegado();
+            }
+        } else if (request.contains("/historico.html") || request.contains("/historico")) {
+            salvarRequisicoes("/historico");
+            if (this.isLogado) {
+                this.output.write(headers.BasicHeader().getBytes());
+                arq = new Arquivo(this.pathToHtml + "/src/html/historico.html");
+                String openFile = arq.openFile();
+                String info = getInfoRequisicoes();
+                String replace = openFile.replace("panzerkampfwagen", info);
+                this.output.write(replace.getBytes());
+            } else {
+                acessoNegado();
+            }
+        } else {
+            arq = new Arquivo(this.pathToHtml + "/src/html/erro404.html");
+            this.output.write(headers.BasicHeader().getBytes());
+            this.output.write(arq.openFile().getBytes());
+
+        }
+    }
+
+    private void metodoPOST(String request, BufferedReader bufferedReader) {
+        try {
+            if (request.contains("/loginFunction")) {
+                this.output.write(headers.BasicHeader().getBytes());
+                arq = new Arquivo(this.pathToHtml + "/src/html/blank.html");
+                this.output.write(arq.openFile().getBytes());
+                fazLogin(bufferedReader);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(OrdoProcessios.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
