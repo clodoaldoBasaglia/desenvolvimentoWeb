68eb8b84b8d75dc9230814b164a8d78f963289a6
clodoaldoBasaglia <clodoaldofonseca92@gmail.com>
2017-11-28 17:19:31 -0200

    sending broadcast

--- WebService/src/webservice/BroadcastSender.java
+++ WebService/src/webservice/BroadcastSender.java
@@ -5,10 +5,13 @@
  */
 package webservice;
 
+import static com.sun.xml.internal.ws.model.RuntimeModeler.PORT;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.InterfaceAddress;
 import java.net.NetworkInterface;
-import java.net.Socket;
 import java.net.SocketException;
 import java.util.Enumeration;
 import java.util.logging.Level;
@@ -19,7 +22,6 @@ import java.util.logging.Logger;
  * @author a968692
  */
 public class BroadcastSender implements Runnable {
-
 
     public BroadcastSender() {
     }
@@ -61,7 +63,31 @@ public class BroadcastSender implements Runnable {
         System.out.println("print do broads");
         InetAddress broadcastIp = getBroadcastIp();
         if (broadcastIp != null) {
-            System.out.println(broadcastIp.toString());
+            sendBroadCast(broadcastIp.toString());
+            System.out.println("this" + broadcastIp.toString());
+        }
+    }
+
+    private void sendBroadCast(String toString) {
+         // Get the address that we are going to connect to.
+        InetAddress addr = InetAddress.getByName(INET_ADDR);
+
+        // Open a new DatagramSocket, which will be used to send the data.
+        try (DatagramSocket serverSocket = new DatagramSocket()) {
+            for (int i = 0; i < 100; i++) {
+                String msg = "Sent message no " + i;
+
+                // Create a packet that will contain the data
+                // (in the form of bytes) and send it.
+                DatagramPacket msgPacket = new DatagramPacket(msg.getBytes(),
+                        msg.getBytes().length, addr, PORT);
+                serverSocket.send(msgPacket);
+
+                System.out.println("Server sent packet with msg: " + msg);
+                Thread.sleep(500);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 
